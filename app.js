186f57bb2041f9a03f52b63484bd7fed5a753aa7
Damarreindra <damarihya08@gmail.com>
2024-10-04 16:27:53 +0700

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');  // Import the cors middleware
+const cors = require('cors');
 require('dotenv').config();
 const playerRoutes = require('./Routes/playerRoute');
 const matchRoutes = require('./Routes/matchRoute');
 
 const app = express();
-const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGODB_URI;
 
 // Connect to MongoDB
@@ -19,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['exp://192.168.100.5:8081', 'http://localhost:3000', 'http://192.168.x.x:3000','exp://192.168.x.x:19000'],
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 204,
 };
@@ -35,6 +34,8 @@ app.use('/api/players', playerRoutes);
 app.use('/api/game', matchRoutes);
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(() => {
+  console.log('Server is running');
 });
+
+module.exports = app;
